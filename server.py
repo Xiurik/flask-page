fb386b51fcb9a74fe878278bbf9f961506179240
Irwin Romero <irwin.romero.rdz@gmail.com>
2022-06-08 18:00:04 -0500

    add: space template

--- server.py
+++ server.py
@@ -3,9 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main_page():
-    return "<h1>Hello, Xiurik!</h1>"
+def home():
+    return render_template('index.html')
 
-@app.route("/template")
-def template():
-    return render_template('index/index.html')
+@app.route("/work")
+def work():
+    return render_template('work.html')
+
+@app.route("/works")
+def works():
+    return render_template('works.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/components")
+def components():
+    return render_template('components.html')
