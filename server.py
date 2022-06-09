d7601a7e67aa442ae1e51d63e2ca521cb8ce9156
Irwin Romero <irwin.romero.rdz@gmail.com>
2022-06-08 19:07:31 -0500

    add: submit form

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
 
+# region 'Imports'
+from flask import Flask, redirect, render_template, request
+
+# endregion
+
+# region 'Load'
 app = Flask(__name__)
+# endregion
 
+# region 'Routing'
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -25,3 +32,21 @@ def contact():
 @app.route("/components")
 def components():
     return render_template('components.html')
+
+@app.route("/thanks")
+def thanks():
+    return render_template('thanks.html')
+# endregion
+
+# region 'LogIn'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        #? Now data is a dictionary like this:
+        #? {'email': 'irwin.romero.rdz@gmail.com', 'subject': 'testing', 'message': 'testing mesage'}
+        print(data)
+        return redirect('thanks')
+    else:
+        return 'Something went wrong'
+# endregion
